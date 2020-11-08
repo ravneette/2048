ed15c778eada3705eaf7b017fcd8ad9a8dd7806d
ravneet <ravkaur93@gmail.com>
2020-11-08 21:39:53 +0530

    2d array initialised

--- 2048.js
+++ 2048.js
@@ -19,3 +19,38 @@ document.addEventListener('keydown', logKey);
 
       }
     }
+
+// Create one dimensional array 
+var grid = new Array(4); 
+  
+// Loop to create 2D array using 1D array 
+document.write("This is what the grid looks like <br><br>"); 
+for (var i = 0; i < grid.length; i++) { 
+    grid[i] = []; 
+} 
+  
+// Loop to initilize 2D array elements. 
+for (var i = 0; i < 4; i++) { 
+    for (var j = 0; j < 4; j++) { 
+  
+        grid[i][j] = parseInt(Math.random() * 3);  // 0 , 1 , 2 , 3 
+
+        if(grid[i][j] === 1) {
+          grid[i][j] = 4
+        }
+        if(grid[i][j] === 3) {
+          grid[i][j] = 8
+        }
+    } 
+} 
+  
+// Loop to display the elements of 2D array. 
+for (var i = 0; i < 4; i++) { 
+    for (var j = 0; j < 4; j++) 
+  
+    { 
+        document.write(grid[i][j] + " "); 
+    } 
+    document.write("<br>"); 
+  }
+
