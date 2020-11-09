3fec98c1b7ef611acb54e30144cd323ba822af22
ravneet <ravkaur93@gmail.com>
2020-11-10 01:00:51 +0530

    Array manipulation functions defined

--- 2048.js
+++ 2048.js
@@ -1,21 +1,139 @@
-document.addEventListener('keydown', logKey);
-    function logKey(e) {
-      if (`${e.code}` == "ArrowRight") {
-        //code here
-        alert("Right Arrow was pressed");
+// Create one dimensional array 
+var grid = new Array(4); 
+
+// Loop to create 2D array using 1D array 
+document.write("This is what the grid looks like <br><br>"); 
+for (var i = 0; i < grid.length; i++) { 
+    grid[i] = []; 
+} 
+
+// Loop to initilize 2D array elements. 
+for (var i = 0; i < 4; i++) { 
+    for (var j = 0; j < 4; j++) { 
+
+        grid[i][j] = parseInt(Math.random() * 3);  // 0 , 1 , 2 , 3 
+
+        if(grid[i][j] === 1) {
+          grid[i][j] = 4
+        }
+        if(grid[i][j] === 3) {
+          grid[i][j] = 8
+        }
+    } 
+} 
+
+// Loop to display the elements of 2D array. 
+function printGrid (grid4)
+{
+for (var i = 0; i < 4; i++) { 
+  for (var j = 0; j < 4; j++) 
+
+  { 
+      document.write(grid4[i][j] + " "); 
+  } 
+  document.write("<br>"); 
+}
+}
+
+printGrid(grid);
+
+// Function for DOM manipulation on keyboard action
+  document.addEventListener('keydown', logKey);
+
+    function logKey(e) 
+    {
+      if (e.code == "ArrowRight") 
+      {
+        grid = grid.reverse();
+        console.log(grid)
+        var newgrid = finalArray(grid);
+        printGrid(newgrid);
       }
-          if (`${e.code}` == "ArrowLeft") {
-        //code here
-        alert("Left Arrow was pressed");
+
+
+      if (e.code == "ArrowLeft") 
+      {
+        var newgrid = finalArray(grid);
+        printGrid(newgrid);
+
+        /*alert("Left Arrow was pressed");
+        var i, j = 0;
+        for (i = 0; i<4; i++) 
+        {
+          for (j=0; j<3; j++) 
+          {
+           if ((grid[i][j]) === 0)
+            {
+              grid[i][j] = grid[i][j+1];
+              grid[i][j+1] = 0;
+            }
+          }
+        }*/
+
+
+
       }
-          if (`${e.code}` == "ArrowDown") {
+      
+
+      if (e.code == "ArrowDown") {
         //code here
         alert("Down Arrow was pressed");
-
       }
-          if (`${e.code}` == "ArrowUp") {
+      
+      
+      if (e.code == "ArrowUp") {
         //code here
         alert("Up Arrow was pressed");
-
       }
+
+
+    // Loop to display the new elements of 2D array. 
+    // for (var i = 0; i < 4; i++) { 
+    //   for (var j = 0; j < 4; j++) 
+
+    //   { 
+    //       document.write(grid[i][j] + " "); 
+    //   } 
+    //   document.write("<br>"); 
+
+    // }
+  }
+
+
+  // Create fucntion to shift numbers to the left in 1D
+
+  function newArray(grid2)
+  {
+    var a = 0; 
+    var newArr =[];
+  
+    for (i = 0; i < grid2.length; i++)
+    {
+      if (grid2[i] !== 0) 
+      {
+       newArr.push(grid2[i]); 
+      }
+    else {a++}
     }
+
+    for (i=0; i<a; i++) 
+    {
+      newArr.push(0);
+    }
+    return newArr;
+  }
+
+
+ //Fucntion to call the final array
+ function finalArray (grid1)
+ {
+   const ret = [];
+
+   for (var i = 0; i < grid1.length; i++)
+   {
+    const val = newArray(grid1[i]);
+    ret.push(val);
+   }
+
+   return ret;
+  }
